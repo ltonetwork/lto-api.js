0115904b4e8a8a0b75b5de4df283b65a0e9be321
sven4ask <sven@legalthings.io>
2018-05-16 22:45:07 +0200

    Fix tests

--- test/HTTPSignature.spec.ts
+++ test/HTTPSignature.spec.ts
@@ -8,7 +8,7 @@ describe('HTTPSignature', () => {
     it('should create a correct signature header using ed25519', () => {
       const headers = {
         '(request-target)': 'get /test',
-        date: (new Date("April 1, 2018 12:00:00")).toISOString()
+        date: (new Date("April 1, 2018 12:00:00Z")).toISOString()
       };
 
       const account = new Account();
@@ -19,13 +19,13 @@ describe('HTTPSignature', () => {
 
       const httpSign = new HTTPSignature(headers, null);
       httpSign.signWith(account, 'ed25519');
-      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519",headers="(request-target) date",signature="OM7vaPQAj9Vp7jJHpcGT1qeSlz6DTETTksabLz9UOdn0Y5GR6PPGg80TQ3SulkbgAqipPSgKQlwOUj2bz6wJBA=="');
+      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519",headers="(request-target) date",signature="EATYkJPy+NmqbfBy/rdwUMQvLOb9VilfsP1b4DgIpDjj3lWd5fSMcuQ3YuRUjOv9r8e+iW+3BLdBe3JLvqrYBw=="');
     });
 
     it('should create a correct signature header using ed25519-sha256', () => {
       const headers = {
         '(request-target)': 'get /test',
-        date: (new Date("April 1, 2018 12:00:00")).toISOString()
+        date: (new Date("April 1, 2018 12:00:00Z")).toISOString()
       };
 
       const account = new Account();
@@ -36,7 +36,7 @@ describe('HTTPSignature', () => {
 
       const httpSign = new HTTPSignature(headers);
       httpSign.signWith(account);
-      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519-sha256",headers="(request-target) date",signature="tMAxot4iWb8gB4FQ2zqIMfH2Fd8kA9DwSoW3UZPj9f8QlpLX5VvWf314vFnM8MsDo5kqtGzk7XOOy0TL4zVWAg=="');
+      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519-sha256",headers="(request-target) date",signature="SUh9qFhP8PYJqoaVMfcYqgSMxBRS74J7LLsNtlzCpagLOY12+pgHB4ujVfyMy1Lr8saPStBmIFhvwh9k9qz8DA=="');
     });
 
     it('should create a correct signature header with post data using ed25519-sha256', () => {
@@ -46,7 +46,7 @@ describe('HTTPSignature', () => {
 
       const headers = {
         '(request-target)': 'post /test',
-        date: (new Date("April 1, 2018 12:00:00")).toISOString()
+        date: (new Date("April 1, 2018 12:00:00Z")).toISOString()
       };
 
       const account = new Account();
@@ -57,7 +57,7 @@ describe('HTTPSignature', () => {
 
       const httpSign = new HTTPSignature(headers, body);
       httpSign.signWith(account);
-      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519-sha256",headers="(request-target) date digest",signature="sQmLXVxJP3aMai7ZtpVoEihUGkXgro4tsWrtJaOXxM9MBrlI5+Tt20+ItIlvssZBQZsLslGgztdIQHyoozkHAA=="');
+      expect(httpSign.getSignature()).to.eq('keyId="FkU1XyfrCftc4pQKXCrrDyRLSnifX1SMvmx1CYiiyB3Y",algorithm="ed25519-sha256",headers="(request-target) date digest",signature="OdToby5/8OV3XUE1YHgj30+kcaNBWQmXJKn5yMl9DW4+Licn10JjPKaOstHtUcEpVrJjjFAb2FuEu0DOwlulBg=="');
     });
   });
 
