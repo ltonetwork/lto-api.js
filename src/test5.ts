fd8a7e8ca169a1999c39ac33e3d731d3807f4691
AndreaScorza <atlantas95@gmail.com>
2021-12-17 18:37:27 +0100

    first successfull transaction

--- src/test5.ts
+++ src/test5.ts
@@ -25,41 +25,21 @@ const privateKeyBytes = base58.decode(account.getPrivateSignKey())
 import { concatUint8Arrays } from './utils/concat';
 import convert from './utils/convert';
 import { isConditionalExpression } from 'typescript';
+import { binary } from '@lto-network/lto-transactions/dist/parseSerialize';
+import { sign } from 'secp256r1';
 
 // so now we want to concatenate the transaction to be signed as a number of Uint8Array
 // let's start with getting the anchor, the anchor length
 
-var anchor = new String("0e8210e7117568d4a9f5e00656cfd2d943ba7842a0ef94c41a4831dc5bb39f5f");
-// console.log(anchor)
-
-const anchorLength = Uint8Array.from(convert.shortToByteArray(anchor.length));
-console.log('first', anchorLength)
-var anchorLengthDue = new String(64);
-// console.log('last', base58.encode(Uint8Array.from(strToBytes(anchor))));
-
+var anchor = "7b08bcf7db118ebf9247d4665b7896929339394ff60f05886bdb17dc0fb4e7dc";
 var type = [15];
 var version = [3];
 var chainId = new String('T');
-var timestamp =  1639735326723;
+var timestamp =  1639762065696;
 var senderKeyType = new String('ed25519');
 var senderPublicKey = 'AneNBwCMTG1YQ5ShPErzJZETTsHEWFnPWhdkKiHG6VTX';
 var txFee = [35000000];
 
-let x = concatUint8Arrays(
-      Uint8Array.from(type),
-      Uint8Array.from(version),  
-      Uint8Array.from(strToBytes(chainId)),
-      Uint8Array.from(strToBytes(timestamp)), 
-      Uint8Array.from(strToBytes(senderKeyType)), 
-      Uint8Array.from(strToBytes(senderPublicKey)),
-      Uint8Array.from([0b10000101100000111011000000]),  
-      Uint8Array.from([1]), 
-      anchorLength, 
-      Uint8Array.from(strToBytes(anchor))
-);
-
-const signature = nacl.sign.detached(x, privateKeyBytes);
-console.log(base58.encode(signature));
 
 
 let tx = {
@@ -69,12 +49,12 @@ let tx = {
   'senderKeyType': 'ed25519', 
   'senderPublicKey': 'AneNBwCMTG1YQ5ShPErzJZETTsHEWFnPWhdkKiHG6VTX', 
   'fee': 35000000, 
-  'timestamp': 1639735326723, 
-  'anchors': ['y7wEx68oVL4JcHEJjjQBBWjdV9JQsQw6ApDmCstVRMi5pmu3WvFuhDvSzzgJxy8ejWXbFw7gjeRjkJzp3EXfGLD'], 
-  'proofs': ['5VygXpb3ubENr3Pt621SgUSyTQbghmCky8U5dH4bqQn5w5jTxPaPyyLYnLguhf4qryorworHozc2pUYQoNSKio1i']
+  'timestamp': 1639762065696, 
+  'anchors': ['27DwT2ER34qA8mreoswvLvFteeiJieWgJkEZehY4nDcjjdXTiC8gfX5aBFYpET1bqCZ3zgHj4KxLCSPYzqQHzDbx'], 
+  'proofs': ['4PsVCrWYUgtvHXDzi6P26xTQbevFzqJHmxqM1Emg3dpdDiJi4g1FRaqH499DTSMtezv32ypn41te5PtGTSL1PyjL']
 }
 const url = 'https://testnet.lto.network/'
-/*axios.post(
+axios.post(
     'transactions/broadcast', 
     tx, 
     {
@@ -88,30 +68,19 @@ const url = 'https://testnet.lto.network/'
       .catch(function (error) {
         // handle error
         console.log(error);
-    });*/
-    console.log('fom here')
-    console.log(base58.encode(concatUint8Arrays(
-      Uint8Array.from(type),
-      Uint8Array.from(version), 
-      Uint8Array.from(strToBytes(chainId)),
-      Uint8Array.from(convert.longToByteArray(timestamp)),
-      Uint8Array.from([1]),
-      base58.decode(account.getPublicSignKey('base58'))
+    });
 
-      
-      
-      
-      )));
-
-
-
-
-
-
-var sendKeyType = new String('ed25519');
-
-
-var encodedPB = account.getPublicSignKey('dsfsdfsd')
-var decodedPubKey = base58.decode(account.getPublicSignKey('base58'))
-
-console.log(decodedPubKey)
+var binaryTx = concatUint8Arrays(
+  Uint8Array.from(type),
+  Uint8Array.from(version), 
+  Uint8Array.from(strToBytes(chainId)),
+  Uint8Array.from(convert.longToByteArray(timestamp)),
+  Uint8Array.from([1]),
+  base58.decode(account.getPublicSignKey('base58')),
+  Uint8Array.from(convert.longToByteArray(35000000)),
+  Uint8Array.from(convert.shortToByteArray(1)),
+  Uint8Array.from(convert.shortToByteArray(anchor.length)),
+  Uint8Array.from(convert.stringToByteArray(anchor))
+  )
+  const sig = nacl.sign.detached(binaryTx, privateKeyBytes);
+  console.log(base58.encode(sig))
