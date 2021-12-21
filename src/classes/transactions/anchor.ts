ffaf1794b50416d5d7014353eb75b35338a940e4
AndreaScorza <atlantas95@gmail.com>
2021-12-21 15:08:29 +0100

    added txV1 anchor

--- src/classes/transactions/anchor.ts
+++ src/classes/transactions/anchor.ts
@@ -8,20 +8,42 @@ import crypto from "../../utils/crypto";
 
 export {Anchor}
 
+const TYPE: number = 15;
+const DEFAULT_FEE: number = 35000000
+const DEFAULT_VERSION: number = 3
+
 class Anchor extends Transaction{
 
+
     anchor: string;
-    txFee: number = 35000000;
-    version: number = 3;
-    type: number = 15;
+    txFee: number;
+    version: number;
+    id: string;
+    height: string;
+    type: number;
+
 
     constructor(anchor: string) {
         super();
         this.anchor = anchor;
+        this.type = TYPE
+        this.txFee = DEFAULT_FEE
+        this.version = DEFAULT_VERSION
     }
 
-
-    toBinary(){
+    toBinaryV1(){
+        return concatUint8Arrays(
+            Uint8Array.from([this.type]),
+            Uint8Array.from([this.version]), 
+            base58.decode(this.senderPublicKey),
+            Uint8Array.from(convert.shortToByteArray(1)),
+            Uint8Array.from(convert.shortToByteArray(this.anchor.length)),
+            Uint8Array.from(convert.stringToByteArray(this.anchor)),
+            Uint8Array.from(convert.longToByteArray(this.timestamp)),
+            Uint8Array.from(convert.longToByteArray(this.txFee))
+            )
+    }
+    toBinaryV3(){
         return concatUint8Arrays(
             Uint8Array.from([this.type]),
             Uint8Array.from([this.version]), 
@@ -35,7 +57,17 @@ class Anchor extends Transaction{
             Uint8Array.from(convert.stringToByteArray(this.anchor))
             )
     }
-
+    toBinary() {
+        switch (this.version) {
+            case 1:
+                return this.toBinaryV1();
+            case 3:
+                return this.toBinaryV3();
+            default:
+                console.error("Incorrect version")
+            
+        }
+    }
     toJson() {
         return(
             {
@@ -54,7 +86,16 @@ class Anchor extends Transaction{
     fromData(data){
         var tx = new Anchor('');
         tx.type = data.type;
-        return 'hello';
+        'id' in data ? (tx.id = data['id']): (tx.id = "");
+        'sender' in data ? (tx.sender = data['sender']) : (tx.sender = '')
+        'senderKeyType' in data ? (tx.senderKeyType = data['senderKeyType']) : (tx.senderKeyType = "ed25519")
+        tx.senderPublicKey = data['senderPublicKey']
+        tx.txFee = data['fee']
+        tx.timestamp = data['timestamp']
+        tx.anchor = data['anchors']
+        'proofs' in data ? (tx.proofs = data['proofs']) : (tx.proofs = [])
+        'height' in data ? (tx.height = data['height']) : (tx.height = '')
+        return tx;
     }
 
 }
--- src/test9.ts
+++ src/test9.ts
@@ -17,7 +17,7 @@ async function my(){
     let ret = await transaction.broadcastTo(node);
     console.log(ret)
 }
-my();
+//my();
 
 
 
@@ -41,13 +41,7 @@ let data = {
       '4PsVCrWYUgtvHXDzi6P26xTQbevFzqJHmxqM1Emg3dpdDiJi4g1FRaqH499DTSMtezv32ypn41te5PtGTSL1PyjL'
     ]
   }
-/*console.log(data['type'])
-switch(data.type) {
-    case 15:
-      console.log("is the anchor")
-      break;
-    case 5:
-        break;
-    default:
-      console.error("Transaction type not recognized")
-  }*/
+
+let res = new Anchor("").fromData(data)
+console.log(res);
+console.log('senderKeyType' in data)
