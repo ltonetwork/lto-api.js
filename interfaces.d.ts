8763cf043c14ef13af5534488a4d77acf9d398fc
Bruno Duarte Brito <brunodb3@gmail.com>
2022-03-03 15:56:42 +0000

    Fixing TBuffer types

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,68 +1,67 @@
-export type TBuffer = Uint8Array | number[];
+export type TBuffer = number[];
 
 export interface IHash<T> {
-    [key: string]: T;
+  [key: string]: T;
 }
 
 export interface IAccountIn {
-    address?: string;
-    publicKey?: string;
-    privateKey?: string;
-    keyType?: "ed25519"|"secp256k1"|"secp256r1";
-    seed?: string;
-    seedPassword?: string,
-    nonce?: number;
+  address?: string;
+  publicKey?: string;
+  privateKey?: string;
+  keyType?: "ed25519" | "secp256k1" | "secp256r1";
+  seed?: string;
+  seedPassword?: string;
+  nonce?: number;
 }
 
 export interface IPublicAccount {
-    keyType: string;
-    publicKey: string;
+  keyType: string;
+  publicKey: string;
 }
 
 export interface IKeyPair {
-    privateKey?: string;
-    publicKey: string;
+  privateKey?: string;
+  publicKey: string;
 }
 
 export interface IBinary extends Uint8Array {
-    readonly base58: string;
-    readonly base64: string;
-    readonly hex: string;
-    toString(): string;
+  readonly base58: string;
+  readonly base64: string;
+  readonly hex: string;
+  toString(): string;
 }
 
 export interface IKeyPairBytes {
-    privateKey?: IBinary;
-    publicKey: IBinary;
+  privateKey?: IBinary;
+  publicKey: IBinary;
 }
 
 export interface ITxJSON extends IHash<any> {
-    type: number;
+  type: number;
 }
 
 export interface ITransfer {
-    recipient: string;
-    amount: number;
+  recipient: string;
+  amount: number;
 }
 
 // Missing interfaces
 declare global {
-    interface Window {
-        msCrypto?: any;
-        Response?: any;
-        Promise: PromiseConstructor;
-    }
+  interface Window {
+    msCrypto?: any;
+    Response?: any;
+    Promise: PromiseConstructor;
+  }
 
-    interface ErrorConstructor {
-        captureStackTrace(thisArg: any, func: any): void;
-    }
+  interface ErrorConstructor {
+    captureStackTrace(thisArg: any, func: any): void;
+  }
 }
 
-
 // Replacement for --allowJs
-declare module '*.js' {
-    const content: {
-        [key: string]: any;
-    };
-    export = content;
+declare module "*.js" {
+  const content: {
+    [key: string]: any;
+  };
+  export = content;
 }
