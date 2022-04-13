f095057dd4e5efee8c9cfdb8e9bafee0857d364a
Arnold Daniels <arnold@jasny.net>
2022-04-13 22:46:46 +0200

    Fix tests. Restore `VerificationRelationship` enum that got removed.

--- src/identities/index.ts
+++ src/identities/index.ts
@@ -1,1 +1,10 @@
 export {default as IdentityBuilder} from "./IdentityBuilder";
+
+export enum VerificationRelationship {
+    none = 0x100,
+    authentication = 0x101,
+    assertion = 0x102,
+    keyAgreement = 0x104,
+    capabilityInvocation = 0x108,
+    capabilityDelegation = 0x110
+}
--- test/accounts/ed25519.spec.ts
+++ test/accounts/ed25519.spec.ts
@@ -54,18 +54,4 @@ describe('ed25519 account', () => {
           .to.throw(new DecryptError('Unable to decrypt message with given keys'));
     })
   });
-
-  describe('#createEventChain', () => {
-    it('should create an account with a random seed', () => {
-      const ec = account.createEventChain();
-      expect(crypto.verifyEventId(ec.id)).to.be.true;
-    });
-
-    it('should create an account with a predefined seed', () => {
-      const nonce = '10';
-
-      const ec = account.createEventChain(nonce);
-      expect(ec.id).to.eq('2bGCW3XbfLmSRhotYzcUgqiomiiFL7y3h7xPH8Ha3FVaYFp6YsjzSRSBeVjnae');
-    });
-  });
 });
