3a50574e9f881f90d316ab4aa4f61a9a3073080f
arnold <arnold@jasny.net>
2022-10-11 08:17:16 -0400

    Added `partial()` method to event chain. Added dynamic `anchorMap` property to event chain.

--- src/events/EventChain.ts
+++ src/events/EventChain.ts
@@ -52,8 +52,12 @@ export default class EventChain {
 
 	public get subject(): Binary {
 		return this.events.length == 0
-			? new Binary(Binary.fromBase58(this.id).reverse()).hash()
+			? this.initialSubject
 			: this.events.slice(-1)[0].subject;
+	}
+
+	private get initialSubject(): Binary {
+		return new Binary(Binary.fromBase58(this.id).reverse()).hash();
 	}
 
 	public set(data: Partial<IEventChainJSON>): EventChain {
@@ -89,7 +93,20 @@ export default class EventChain {
 	}
 
 	public isSigned(): boolean {
-		return this.events.reduce((b, e) => b && e.isSigned(), true);
+		return this.events.every(e => e.isSigned());
+	}
+
+	public partial(start: Binary) {
+		const index = this.events.findIndex(e => e.hash.hex === start.hex);
+
+		if (index < 0) {
+			throw new Error(`Event ${start} is not part of this event chain`);
+		}
+
+		const chain = new EventChain(this.id);
+		chain.events = this.events.slice(index);
+
+		return chain;
 	}
 
 	public isPartial(): boolean {
@@ -98,6 +115,22 @@ export default class EventChain {
 
 	public isCreatedBy(account: ISigner): boolean {
 		return crypto.verifyEventChainId(EVENT_CHAIN_VERSION, this.id, Binary.fromBase58(account.publicKey));
+	}
+
+	public get anchorMap(): Array<{key: Binary, value: Binary}> {
+		const map: Array<{key: Binary, value: Binary}> = [];
+		let subject = this.initialSubject;
+
+		for (const event of this.events) {
+			map.push({key: subject, value: event.hash});
+			subject = event.subject;
+		}
+
+		if (this.isPartial()) {
+			map.shift(); // Subject of the first event is unknown in case of a partial event chain
+		}
+
+		return map;
 	}
 
 	public static from(data: IEventChainJSON[]): EventChain {
