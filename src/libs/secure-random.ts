2551805e8bfa2ad40c9b2a2a8a5541b894441fd5
arnold <arnold@jasny.net>
2023-06-19 13:01:45 -0400

    Use webcrypto `getRandomValues()` if available (also node 20+). Fall back to nodejs `crypto`, only when using node. For other environments (like react native), you should polyfill `getRandomValues` and not node `crypto`.

--- src/libs/secure-random.ts
+++ src/libs/secure-random.ts
@@ -1,18 +1,28 @@
 // @ts-nocheck
 declare let require: any;
 
-function nodeRandom(count): Uint8Array {
-  const crypto = require(/* webpackIgnore: true */ 'crypto');
-  return new Uint8Array(crypto.randomBytes(count).buffer);
-}
-
-function browserRandom(count): Uint8Array {
-  const nativeArr = new Uint8Array(count);
-  const crypto = self.crypto || self.msCrypto;
-  crypto.getRandomValues(nativeArr);
-  return nativeArr;
-}
-
 export function randomUint8Array(count: number): Uint8Array {
-  return typeof window !== 'undefined' || typeof self !== 'undefined' ? browserRandom(count) : nodeRandom(count);
+  const webcrypto = typeof self !== 'undefined' && typeof self.crypto !== 'undefined' ? self.crypto
+    : typeof globalThis !== 'undefined' && typeof globalThis.crypto !== 'undefined' ? globalThis.crypto
+    : typeof crypto !== 'undefined' ? crypto
+    : {};
+
+  if (typeof webcrypto.getRandomValues !== 'undefined') {
+    const nativeArr = new Uint8Array(count);
+    webcrypto.getRandomValues(nativeArr);
+    return nativeArr;
+  }
+
+  const isNode =
+    typeof process !== "undefined" &&
+    process.versions != null &&
+    process.versions.node != null;  
+  
+  if (isNode) {
+    const crypto = require(/* webpackIgnore: true */ 'crypto');
+    return new Uint8Array(crypto.randomBytes(count).buffer);
+  }
+  
+  throw new Error("crypto.getRandomValues not supported. Please use a polyfill.");
 }
+
