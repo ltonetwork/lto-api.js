18161a0872b3027f93f4302593dd64979c397987
arnold <arnold@jasny.net>
2023-07-18 21:07:05 -0400

    Export as individual submodules. Allows for tree-shaking. Fixes #112
    [bump:minor]

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,3 @@ export default LTO;
 export { LTO };
 
 export { default as Binary } from './Binary';
-
-export * from './accounts';
-export * from './errors';
-export * from './events';
-export * from './identities';
-export * from './messages';
-export * from './node';
-export * from './transactions';
-export * from './utils';
